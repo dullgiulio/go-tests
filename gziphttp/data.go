1b400a4187973f0bb35726cedf466d1883075d6d
Giulio Iotti <dullgiulio@gmail.com>
2015-08-25 12:29:20 +0300

    Add gzip recycler text

---
+++ gziphttp/data.go
@@ -0,0 +1,103 @@
+package main
+
+var data []byte = []byte(`var __dmtb32 = {"cvalue":22218,"js":[{"descr":"muachung.vn","image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg
+    \/2015\/08\/goi-c-155c973cdbb419.jpg","cocode":1804679530,"cpa":0,"link3rd":"","link":"http:\/\/muachung
+    .vn\/thoi-trang-nu\/goi-cam-hon-voi-ao-lot-duc-3-day-87391.html?utm_source=admarket&utm_medium=acquireuser
+    &utm_campaign=cpc&utm_deal=87391","rank":10,"bid":342485,"title":"Gợi cảm hơn với áo lót đúc 3 dây <b
+     style=\"font-weight:bold;color:#fc0203;\">-40%<\/b>","l":"4","content":"Chất liệu mềm mại, thấm hút
+    , <b>không bức bí<\/b>. Cho vòng 1 quyến rũ, tròn đầy. Chỉ <b style=\"font-weight:bold;color:#fc0203
+    ;\">120.000đ<\/b>!","cid":1064618},{"descr":"muachung.vn","image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg
+    \/2015\/06\/combo-155780b3e53f6a.jpg","cocode":1330287696,"cpa":0,"link3rd":"","link":"http:\/\/muachung
+    .vn\/thoi-trang-nu\/combo-3-quan-lot-duc-khong-duong-may-88198.html?utm_source=admarket&utm_medium=acquireuser
+    &utm_campaign=cpc&utm_deal=88198","rank":10,"bid":334200,"title":"3 quần lót đúc không đường may <b style
+    =\"font-weight:bold;color:#fc0203;\">-39%<\/b>","l":"4","content":"Hàng <b>Việt Nam<\/b>, kiểu dáng
+    đơn giản, không vết hằn, phù hợp với nhiều lứa tuổi. <b style=\"font-weight:bold;color:#fc0203;\">110
+    .000đ!<\/b>","cid":1064618},{"descr":"muachung.vn","image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg\/2015
+    \/06\/Thoa--1557657e42f986.jpg","cocode":414233720,"cpa":0,"link3rd":"","link":"http:\/\/muachung.vn
+    \/dao-tao-giai-tri\/vui-choi-an-trua-cao-cap-tai-flamingo-dai-lai-5-89568.html?utm_source=admarket&utm_medium
+    =acquireuser&utm_campaign=cpc&utm_deal=89568","rank":10,"bid":333949,"title":"Vui chơi, ăn trưa Flamingo
+     Đại Lải <b style=\"font-weight:bold;color:#fc0203;\">-50%<\/b>","l":"4","content":"Điểm dã ngoại ngo
+    ài trời <b style=\"font-weight:bold;color:#fc0203;\">5*<\/b> & nhiều dịch vụ, hoạt động thể thao đa d
+    ạng. <b style=\"font-weight:bold;color:#fc0203;\">735.000đ<\/b><b>\/người!<\/b>","cid":1064618},{"descr"
+    :"muachung.vn","image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg\/2015\/07\/vay-2-155a73b87bc078.jpg","cocode"
+    :143252772,"cpa":0,"link3rd":"","link":"http:\/\/muachung.vn\/thoi-trang-nu\/vay-2-day-co-yem-cach-dieu-duyen-dang-cho-ban-gai-100047
+    .html?utm_source=admarket&utm_medium=acquireuser&utm_campaign=cpc&utm_deal=100047","rank":10,"bid":339090
+    ,"title":"Váy 2 dây cổ yếm cách điệu <b style=\"font-weight:bold;color:#fc0203;\">-34%<\/b>","l":"4"
+    ,"content":"Chất liệu voan cát thoáng mát, kiểu dáng 2 dây mát mẻ, trẻ trung. <b>Giảm<\/b> <b>còn<\/b
+    > <b style=\"font-weight:bold;color:#fc0203;\">99.000đ<\/b>!","cid":1064618},{"descr":"www.lazada.vn"
+    ,"image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg\/2015\/07\/bo-2--155953a9d67d75.jpg","cocode":441860953
+    ,"cpa":1,"link3rd":"","link":"http:\/\/www.lazada.vn\/bo-2-khoa-chong-trom-thong-minh-kinbar-den-va-bac-1080986
+    .html?wt_dp_l=vn.display_local.admarket.[am]_0600000000_KI002HLAX63EVNAMZ-636151_khoatrongchongkinbar
+    ..&utm_source=admarket&utm_medium=display_local&utm_campaign=[am]_0600000000_KI002HLAX63EVNAMZ-636151_khoatrongchongkinbar_
+    &utm_content=","rank":10,"bid":337122,"title":"Bộ 2 khóa chống trộm thông minh Kinbar","l":"1,2,3","content"
+    :"Phát ra còi báo động lên đến 110dB. Giá chỉ còn: 339.000đ. MUA NGAY!","cid":1065462},{"descr":"www
+    .lazada.vn","image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg\/2015\/08\/56922-155cd3ec9ccdb2.jpg","cocode"
+    :62581766,"cpa":1,"link3rd":"","link":"http:\/\/www.lazada.vn\/may-khau-gia-dinh-mini-muacungh2t-1140-maykhau-298764
+    .html?wt_dp_l=vn.display_local.admarket.[am]_05000000000_MU912HLAGEJ0VNAMZ-330654_maykhau2thot..&utm_source
+    =admarket&utm_medium=display_local&utm_campaign=[am]_05000000000_MU912HLAGEJ0VNAMZ-330654_maykhau2thot_
+    &utm_content=","rank":10,"bid":342887,"title":"Máy khâu gia đình mini - giảm <b style=\"font-weight:bold
+    ;color:#fc0203;\">HOT<\/b>","l":"7,4,31,5","content":"May vá được tất cả các loại vải từ vải mềm cho
+     đến vải thô, vải cứng. Giá còn: <b style=\"font-weight:bold;color:#fc0203;\">259.000đ<\/b>","cid":1064591
+    },{"descr":"www.lazada.vn","image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg\/2015\/06\/black-1557070280c52e
+    .jpg","cocode":1498804427,"cpa":1,"link3rd":"","link":"http:\/\/www.lazada.vn\/bo-dung-cu-may-khoan-va-138-chi-tiet-skil-6613-47035
+    .html?wt_dp_l=vn.display_local.admarket.[am]_06000000000_BL200HLAEJFBVNAMZ_khoanblackdeckergroupsoc.
+    .&utm_source=admarket&utm_medium=display_local&utm_campaign=[am]_06000000000_BL200HLAEJFBVNAMZ_khoanblackdeckergroupsoc_
+    &utm_content=","rank":10,"bid":333585,"title":"Bộ máy khoan búa BLACK & DECKER-giảm <b style=\"font-weight
+    :bold;color:#fc0203;\">SỐC<\/b>","l":"1,2,3","content":"Bộ khoan búa từ Mỹ khiến việc sửa chữa trở n
+    ên dễ dàng. Duy nhất tại Lazada:<b style=\"font-weight:bold;color:#fc0203;\">609.000đ<\/b>.CLICK","cid"
+    :1064591},{"descr":"www.goldmarkcityvn.com","image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg\/2015\/08\
+    /Sanhd-155c967e93e3d3.jpg","cocode":2002225743,"cpa":0,"link3rd":"","link":"http:\/\/www.goldmarkcityvn
+    .com\/2015\/08\/tong-quan-du-goldmark-city.html","rank":10,"bid":342475,"title":"Mua căn hộ đẹp nhất
+     Hồ Tùng Mậu","l":"4","content":"Chỉ từ <b style=\"font-weight:bold;color:#fc0203;\">2 tỷđ\/<\/b>căn
+     2PN. Trúng ngay siêu xe Mercedes. 0917.124.468","cid":1067055},{"descr":"ecogreencitychungcu.net","image"
+    :"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg\/2015\/08\/Inter-155c9a4db28c40.jpg","cocode":2009226020,"cpa"
+    :0,"link3rd":"","link":"http:\/\/ecogreencitychungcu.net\/","rank":10,"bid":342534,"title":"Căn hộ g
+    ần trung tâm Q. Thanh Xuân","l":"4","content":"Chỉ từ <b style=\"font-weight:bold;color:#fc0203;\">1
+    .5 tỷđ\/căn<\/b> (2PN, gồm VAT + full Nội thất) LH 0932363753","cid":1067077},{"descr":"www.lazada.vn"
+    ,"image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg\/2015\/07\/camer-1559a46a4c06d6.jpg","cocode":596727006
+    ,"cpa":1,"link3rd":"","link":"http:\/\/www.lazada.vn\/camera-hanh-trinh-hd-dvr-grentech-den-363814.html
+    ?wt_dp_l=vn.display_local.admarket.[am]_04000000000_GR713ELAHSPYVNAMZ-411536_grentechcamera0507..&utm_source
+    =admarket&utm_medium=display_local&utm_campaign=[am]_04000000000_GR713ELAHSPYVNAMZ-411536_grentechcamera0507_
+    &utm_content=","rank":10,"bid":337509,"title":"Camera hành trình HD DVR Grentech -<b style=\"font-weight
+    :bold;color:#fc0203;\">HOT<\/b>","l":"4","content":"Hỗ trợ hồng ngoại ban đêm, ghi hình ở mọi thời ti
+    ết. Giá còn: 449.000đ. MUA GẤP","cid":1064591},{"descr":"www.lazada.vn","image":"\/\/adi.vcmedia.vn\
+    /adt\/cpc\/ssvimg\/2015\/07\/bo-2--155b65aaf4f518.jpg","cocode":441860953,"cpa":1,"link3rd":"","link"
+    :"http:\/\/www.lazada.vn\/bo-2-khoa-chong-trom-thong-minh-kinbar-den-va-bac-1080986.html?wt_dp_l=vn.display_local
+    .admarket.[am]_0600000000_KI002HLAX63EVNAMZ-636151_khoatrongchongkinbar-lowcir..&utm_source=admarket
+    &utm_medium=display_local&utm_campaign=[am]_0600000000_KI002HLAX63EVNAMZ-636151_khoatrongchongkinbar-lowcirb2s_
+    &utm_content=","rank":10,"bid":340635,"title":"Bộ 2 khóa chống trộm Kinbar-giảm cực<b style=\"font-weight
+    :bold;color:#fc0203;\"> SỐC<\/b>","l":"7,4,31,5","content":"Phát ra còi báo động lên đến 110dB. Giá ch
+    ỉ còn: 369.000đ. NHẬP MÃ VOUCHER để giảm thêm","cid":1066537},{"descr":"www.lazada.vn","image":"\/\
+    /adi.vcmedia.vn\/adt\/cpc\/ssvimg\/2015\/07\/camer-155b663cf024e9.jpg","cocode":596727006,"cpa":1,"link3rd"
+    :"","link":"http:\/\/www.lazada.vn\/camera-hanh-trinh-hd-dvr-grentech-den-363814.html?wt_dp_l=vn.display_local
+    .admarket.[am]_04000000000_GR713ELAHSPYVNAMZ-411536_grentechcamera0507-lowcir..&utm_source=admarket&utm_medium
+    =display_local&utm_campaign=[am]_04000000000_GR713ELAHSPYVNAMZ-411536_grentechcamera0507-lowcir_&utm_content
+    =","rank":10,"bid":340638,"title":"Camera hành trình HD DVR Grentech -<b style=\"font-weight:bold;color
+    :#fc0203;\">HOT<\/b>","l":"4","content":"Chỉ:<b style=\"font-weight:bold;color:#fc0203;\"> 449.000đ<
+    \/b>- hỗ trợ hồng ngoại ban đêm, ghi hình trên xe ở mọi điều kiện thời tiết.","cid":1066537},{"descr"
+    :"muachung.vn","image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg\/2015\/07\/o-cam-155a734383680f.jpg","cocode"
+    :503412683,"cpa":0,"link3rd":"","link":"http:\/\/muachung.vn\/dien-tu-cong-nghe\/o-cam-dien-an-toan-nakagami-8-lo-day-dai-2m-79398
+    .html?utm_source=admarket&utm_medium=acquireuser&utm_campaign=cpc&utm_deal=79398","rank":10,"bid":339073
+    ,"title":"Ổ cắm điện an toàn Nakagami <b style=\"font-weight:bold;color:#fc0203;\">-43%<\/b>","l":"4"
+    ,"content":"<b>Tiện lợi<\/b> với 8 lỗ, dây dài 2m; tự động ngắt điện khi vượt tải cho phép. <b>Giảm<
+    \/b> còn <b style=\"font-weight:bold;color:#fc0203;\">120.000<\/b>đ!","cid":1064618},{"descr":"www.lazada
+    .vn","image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg\/2015\/07\/bo-2--1559becc128eb1.jpg","cocode":441860953
+    ,"cpa":1,"link3rd":"","link":"http:\/\/www.lazada.vn\/bo-2-khoa-chong-trom-thong-minh-kinbar-den-va-bac-1080986
+    .html?wt_dp_l=vn.display_local.admarket.[am]_06000000000_KI002HLAX63EVNAMZ-636151_khoankinbar1208..&utm_source
+    =admarket&utm_medium=display_local&utm_campaign=[am]_06000000000_KI002HLAX63EVNAMZ-636151_khoankinbar1208_
+    &utm_content={{URL_REFERAL}}","rank":10,"bid":337734,"title":"Bộ 2 khóa chống trộm Kinbar - giảm <b style
+    =\"font-weight:bold;color:#fc0203;\">HOT<\/b>","l":"1,2,3","content":"Phát ra còi báo động lên đến 110dB
+    . Giá còn: <b style=\"font-weight:bold;color:#fc0203;\">339.000đ<\/b>. Nhập mã voucher để giảm thêm
+    !","cid":1065462},{"descr":"nhavang.vn","image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg\/2015\/08\/unnam-155c9b5b923727
+    .jpg","cocode":652409976,"cpa":0,"link3rd":"","link":"http:\/\/nhavang.vn\/du-an\/ct1a-ct1b-nghia-do"
+    ,"rank":10,"bid":342598,"title":"Bán căn hộ trung tâm Hoàng Quốc Việt","l":"4","content":"View Hồ Tây
+    , ghép căn sáng tạo. Giá chỉ từ <b style=\"font-weight:bold;color:#fc0203;\">1,25 tỷđ<\/b>\/căn. LH:
+      0914.12.13.99","cid":1067096},{"descr":"www.lazada.vn","image":"\/\/adi.vcmedia.vn\/adt\/cpc\/ssvimg
+    \/2015\/07\/en-le-1559bc001340c9.jpg","cocode":1997903911,"cpa":1,"link3rd":"","link":"http:\/\/www.lazada
+    .vn\/nha-cua-doi-song\/?ref=HL\/?skus[]=RI872HLAWKZ3VNAMZ&skus[]=KA590HLAUPQUVNAMZ&skus[]=TO928HLAW2HJVNAMZ
+    &skus[]=KA590HLAUPQHVNAMZ&skus[]=RI872HLAWKZ3VNAMZ&wt_dp_l=vn.display_local.admarket.[am]_06000000000_none_groupdendenledmauvang0707soc
+    ..&utm_source=admarket&utm_medium=display_local&utm_campaign=[am]_06000000000_none_groupdendenledmauvang0707soc_
+    &utm_content=","rank":10,"bid":337703,"title":"Đèn LED chống cận hiện đại - giảm <b style=\"font-weight
+    :bold;color:#fc0203;\">SỐC<\/b>","l":"7,4,31,5","content":"Chỉ <b style=\"font-weight:bold;color:#fc0203
+    ;\">489.000đ<\/b> - 3 chế độ Đèn ngủ, Đèn học, Đèn vẽ mỹ thuật. Mua đèn giá HOT tại LAZADA.","cid":1065462
+    }],"dspid":3,"zfr":56,"ext":{"logo": 0}, "dsprq": { 22:9848, 3:2266},"zid":2266};`)
---
+++ gziphttp/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"compress/gzip"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+	"runtime/pprof"
+	"time"
+)
+
+type deferredWriter struct {
+	io.Writer
+}
+
+func newDeferredWriter() *deferredWriter {
+	return &deferredWriter{ioutil.Discard}
+}
+
+func (d *deferredWriter) setWriter(w io.Writer) {
+	d.Writer = w
+}
+
+type deferredGzip struct {
+	*gzip.Writer
+	w *deferredWriter
+}
+
+func newDeferredGzip(w *deferredWriter) *deferredGzip {
+	gw, _ := gzip.NewWriterLevel(w, gzip.BestSpeed)
+	return &deferredGzip{gw, w}
+}
+
+func (dg *deferredGzip) setWriter(w io.Writer) {
+	dg.w.Writer = w
+}
+
+func (dg *deferredGzip) reset() {
+	dw := newDeferredWriter()
+	dg.Reset(dw)
+	dg.w = dw
+}
+
+type handler struct {
+	zips chan *deferredGzip
+	used chan *deferredGzip
+}
+
+func newHandler(n int) *handler {
+	h := &handler{
+		zips: make(chan *deferredGzip, n),
+		used: make(chan *deferredGzip, n),
+	}
+	for i := 0; i < n; i++ {
+		h.zips <- newDeferredGzip(newDeferredWriter())
+	}
+	return h
+}
+
+func (h *handler) httpHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Encoding", "gzip")
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(200)
+
+	dg := <-h.zips
+	dg.setWriter(w)
+	dg.Write(data)
+	dg.Close()
+	h.used <- dg
+}
+
+func (h *handler) recycle() {
+	for dg := range h.used {
+		dg.reset()
+		h.zips <- dg
+	}
+}
+
+func main() {
+	n := runtime.NumCPU()
+	runtime.GOMAXPROCS(n)
+
+	var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	h := newHandler(n)
+	http.HandleFunc("/", h.httpHandler)
+	go h.recycle()
+
+	if *cpuprofile != "" {
+		go http.ListenAndServe(":8080", nil)
+		<-time.After(3 * time.Second)
+	} else {
+		http.ListenAndServe(":8080", nil)
+	}
+}
